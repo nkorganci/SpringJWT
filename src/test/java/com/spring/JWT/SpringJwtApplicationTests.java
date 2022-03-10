079bea031a09dcd53b3042c1bb9d3c5e6063f042
Nuri Korganci <nkorganci@gmail.com>
2022-03-10 08:59:25 -0600

    Initial commit

---
+++ src/test/java/com/spring/JWT/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
