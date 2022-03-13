adaed11161173b90cfd5282d2e4e6b0c2edda50e
Nuri Korganci <nkorganci@gmail.com>
2022-03-13 07:44:14 -0500

    m

--- src/main/java/com/spring/JWT/model/Kisi.java
+++ src/main/java/com/spring/JWT/model/Kisi.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "personal")
@@ -23,4 +25,9 @@ public class Kisi {
     @NotBlank
     @Email
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "kisi_roller", joinColumns = @JoinColumn(name = "kisi_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<KisiRole> roller = new HashSet<>();
+
 }
